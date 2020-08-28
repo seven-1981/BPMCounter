552bdcc774c11a1812d75bb954373969cd1117e5
seven1981 <d_schlatter@gmx.ch>
2020-08-28 23:53:46 +0200

    Update

--- main.cpp
+++ main.cpp
@@ -59,6 +59,7 @@ int main (int argc, char** argv)
 	std::this_thread::sleep_for(std::chrono::seconds(1));
 
     //w.setBaseSize(800, 600);
+
     w.showFullScreen();
     w.show();
     return a.exec();
