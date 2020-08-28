9af69f17b0766d75922e15252b4d1f94fcce44fe
seven1981 <d_schlatter@gmx.ch>
2020-08-28 23:03:23 +0200

    Added path variable to make script

--- makeall.sh
+++ makeall.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+
+# Get the project path
+PROJ_PATH=$(git rev-parse --show-toplevel)
+echo Current project path = $PROJ_PATH
+
+# Reset the build (erase the build directory)
 reset_build() {
-echo Dir=$1
 BUILD_DIR=$1
 cd $BUILD_DIR
 rm -r build
@@ -8,33 +13,35 @@ mkdir build
 cd build
 }
 
+# Check if a clean command was issued
 if [ "$1" == "clean" ]; then
 	echo Cleaning BPMDetector...
 	reset_build ..
 	exit 0
 fi
 
+# Start the build process
 echo Building BPMDetector...
 echo Build Audio tests...
-reset_build Audio/TestAudio
+reset_build ${PROJ_PATH}/Audio/TestAudio
 cmake ..
 if cmake --build .; then
 ./runTests
 
 echo Build Analyze tests...
-reset_build ../../../Analyze/TestAnalyze
+reset_build ${PROJ_PATH}/Analyze/TestAnalyze
 cmake ..
 if cmake --build .; then
 ./runTests
 
 echo Build Plot...
-reset_build ../../../Plot
+reset_build ${PROJ_PATH}/Plot
 cmake ..
 if cmake --build .; then
 echo Successfully built Plot.
 
 echo Build Application...
-reset_build ../..
+reset_build ${PROJ_PATH}
 cmake ..
 if cmake --build .; then
 echo Successfully built application.
