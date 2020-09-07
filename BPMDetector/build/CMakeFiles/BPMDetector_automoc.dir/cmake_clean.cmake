6cf749c2fd52087035b19a60c679202e1438b34a
seven1981 <d_schlatter@gmx.ch>
2020-09-07 18:49:35 +0200

    Removed BPMDetector subdir

--- BPMDetector/build/CMakeFiles/BPMDetector_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "BPMDetector_automoc.cpp"
-  "CMakeFiles/BPMDetector_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/BPMDetector_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
